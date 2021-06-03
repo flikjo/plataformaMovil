4adee627ea92fc04a171c47482f2b69eb38e4ca5
flikjo <mabeltran21@poligran.edu.co>
2021-06-02 21:17:37 -0500

    ultima version reconocimiento facial

--- Reconocimiento facial/face.py
+++ Reconocimiento facial/face.py
@@ -8,6 +8,10 @@ mp_face_mesh = mp.solutions.face_mesh
 # For webcam input:
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 cap = cv2.VideoCapture(0)
+contUp=0
+contLeft=0
+contRight=0
+contUnder=0
 with mp_face_mesh.FaceMesh(
     max_num_faces=1,
     min_detection_confidence=0.5,
@@ -32,37 +36,60 @@ with mp_face_mesh.FaceMesh(
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     heinght, width, _ =image.shape
+    
     if results.multi_face_landmarks:
       index=[1]
       for face_landmarks in results.multi_face_landmarks:
+        a=int(width/2)
+        b=int(heinght/2)
+        
+        cv2.circle(image,(a,b),60,(255,0,0),1)
         for (i, points) in enumerate(face_landmarks.landmark):
           if i in index:
             #x=points.x
             #y=points.y
+            
             x=int(points.x*width)
             y=int(points.y*heinght)
-            cv2.circle(image,(x,y),3,(255,0,0),3)
+            cv2.circle(image,(x,y),1,(255,0,0),2)
             #print(x , y)
 
-            if(y<210):
-              print("Arriba")
-              enviar(2)
+            if(y<182):
+              if(contUp==0):
+                print("Arriba")
+                #enviar(1)
+                contUp=10
+            elif(y>182):
+              contUp=0
              # print(x , y)
-            if(y>290):
-              print("Abajo")
-              enviar(1)
-            if(x<230):
-              print("Izquierda")
-              enviar(3)
-            if(x>380):
-              print("Derecha")
-              enviar(4)
-        mp_drawing.draw_landmarks(
-            image=image,
-            landmark_list=face_landmarks,
-            connections=mp_face_mesh.FACE_CONNECTIONS,
-            landmark_drawing_spec=drawing_spec,
-            connection_drawing_spec=drawing_spec)
+            if(y>297):
+              if(contUnder==0):
+                print("Abajo")
+                #enviar(0)
+                contUnder=1
+            elif(y<297):
+              contUnder=0
+            if(x<255):
+              if(contLeft==0):
+                print("Izquierda")
+                #enviar(2)
+                contLeft=1
+            elif(x>255):
+                contLeft=0
+            if(x>385):
+              if(contRight==0):
+                print("Derecha")
+                #enviar(3)
+                contRight=1
+            elif(x<385):
+                contRight=0
+                
+       # mp_drawing.draw_landmarks(
+       #     image=image,
+       #     landmark_list=face_landmarks,
+       #     connections=mp_face_mesh.FACE_CONNECTIONS,
+       #     landmark_drawing_spec=drawing_spec,
+       #     connection_drawing_spec=drawing_spec)
 
     cv2.imshow('MediaPipe FaceMesh', image)
     if cv2.waitKey(5) & 0xFF == 27:
